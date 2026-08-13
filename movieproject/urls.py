587cd44ef05d1b3059a1fdabb4839516b4f0ef44
Your Name <dhanasubramanian29@gmail.com>
2026-08-13 11:29:32 +0530

    Fix media image serving

--- movieproject/urls.py
+++ movieproject/urls.py
@@ -34,3 +34,12 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
+    
+    
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("movies.urls")),
+    path("accounts/", include("accounts.urls")),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
